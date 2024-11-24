b25091df1da93088c6b649e5e14fbcc52e2ce77d
Surya Kant Sharma <suryakant@Suryas-MacBook-Air.local>
2024-11-24 21:00:20 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { createAppKit } from '@reown/appkit/react'
+import { WagmiProvider } from 'wagmi'
+import { arbitrum, mainnet, polygon, base } from '@reown/appkit/networks'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
+import { useAppKit } from '@reown/appkit/react'
 
-function App() {
+
+const HApp = () => {
+  const { open, close } = useAppKit()
   return (
     <div className="App">
       <header className="App-header">
@@ -17,8 +25,65 @@ function App() {
         >
           Learn React
         </a>
+
+        <button onClick={() => {
+          open({view : "Connect"})
+        }}>Open Modal</button>
+        <button onClick={() => {
+          open({view :  "Account"})
+        }}>Account</button>
       </header>
     </div>
+  )
+}
+
+function App() {
+
+
+
+// 0. Setup queryClient
+const queryClient = new QueryClient()
+
+// 1. Get projectId from https://cloud.reown.com
+const projectId = ''
+
+// 2. Create a metadata object - optional
+const metadata = {
+  name: 'metamask',
+  description: 'AppKit Example',
+  url: 'https://reown.com/appkit', // origin must match your domain & subdomain
+  icons: ['https://assets.reown.com/reown-profile-pic.png']
+}
+
+// 3. Set the networks
+const networks = [mainnet, base]
+
+// 4. Create Wagmi Adapter
+const wagmiAdapter = new WagmiAdapter({
+  networks,
+  projectId,
+  ssr: true
+});
+
+// 5. Create modal
+createAppKit({
+  adapters: [wagmiAdapter],
+  networks,
+  projectId,
+  metadata,
+  features: {
+    analytics: true // Optional - defaults to your Cloud configuration
+  }
+})
+
+    
+  return (
+    <WagmiProvider config={wagmiAdapter.wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <HApp />
+      </QueryClientProvider>
+    
+    </WagmiProvider>
   );
 }
 
